5011368c89bb03cb63f2fac85d398b65f89fdba1
tsogp <specialforbtsu@gmail.com>
2022-08-15 22:48:23 +0700

    tasks 2.6-2.10 added

--- part2/README.md
+++ part2/README.md
@@ -1,1 +1,2 @@
 exercises 2.1-2.5 are are defined in App.js file
+exercises 2.6-2.10 are are defined in App1.js file
---
+++ part2/src/App1.js
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+
+const Filter = (props) => {
+    return (
+        <>
+            filter shown with <input onChange={props.handleFilterChange} />
+        </>
+    )
+}
+
+const PersonForm = (props) => {
+    return (
+        <form>
+            <div>
+                name: <input onChange={props.handleNameChange} />
+                number: <input onChange={props.handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit" onClick={props.addPerson}>add</button>
+            </div>
+        </form>
+    )
+}
+
+const Persons = ({ persons }) => {
+    return (
+        persons.map(p => 
+            <h3 key={Math.random() * 1000}>{p.name} {p.number}</h3>
+        )
+    )
+}
+
+const App1 = () => {
+    const [persons, setPersons] = useState([{ 
+            name: 'Arto Hellas',
+            number: '8-800-555-35-35'
+        }
+    ]) 
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        if (persons.find(p => p.name === newName)) {
+            alert(`${newName} is already in the phonebook`)
+        } else {
+            const newPerson = {
+                name: newName,
+                number: newNumber
+            }
+
+            setPersons(persons.concat(newPerson))
+        }   
+    }
+
+    return (
+        <div>
+            <h2>Filter</h2>
+            <Filter handleFilterChange={handleFilterChange} />
+
+            <h2>add a new</h2>
+            <PersonForm
+                handleNameChange={handleNameChange}
+                handleNumberChange={handleNumberChange}
+                addPerson={addPerson}
+            />
+
+            <h2>Numbers</h2>
+            <Persons persons={filter ? persons.filter(p => 
+                p.name.toLowerCase().split(' ')[0].search(filter) != -1) : persons} 
+            />
+        </div>
+    )
+}
+
+export default App1
