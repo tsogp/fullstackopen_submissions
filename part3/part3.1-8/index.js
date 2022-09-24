004aff56f4686e641d2705556fab3562d40b4aee
tsogp <specialforbtsu@gmail.com>
2022-09-24 10:21:31 +0700

    exercises 3.1-8

--- part3/part3.1-8/index.js
+++ part3/part3.1-8/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+morgan.token('body', (req) => {
+    return JSON.stringify(req['body'])
+})
+
 const app = express()
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
