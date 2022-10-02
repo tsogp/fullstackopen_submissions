708720527f646c69e54fe6d21ff5b46240ccf5a7
tsogp <specialforbtsu@gmail.com>
2022-10-02 14:21:58 +0700

    build included

---
+++ part3/build/precache-manifest.07c80c3a10122ab6e9ffae003a1532c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "150a4c9f90fca1de4105",
+    "url": "/static/css/main.c78f620d.chunk.css"
+  },
+  {
+    "revision": "150a4c9f90fca1de4105",
+    "url": "/static/js/main.e5a31f86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fc3f848c995c6fcade8",
+    "url": "/static/js/2.53b6e9f8.chunk.js"
+  },
+  {
+    "revision": "d8e11a1ccfc786b45c83614655d51ad6",
+    "url": "/index.html"
+  }
+];
