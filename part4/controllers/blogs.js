994c13f3c5848309281c320eeb1eeb23ac10e739
Pavel Potemkin <specialforbtsu@gmail.com>
2023-04-24 10:20:42 +0700

    4.21

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -36,7 +36,17 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-	await Blog.findByIdAndDelete(request.params.id);
+	const blog = await Blog.findById(request.params.id);
+
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+	if (!decodedToken) {
+		return response.status(401).json({ error: 'token invalid' });
+	} else if (blog && decodedToken.id.toString() != blog.user._id.toString()) {
+		return response.status(401).json({ error: 'invalid blog id' });
+	}
+
+	await blog.delete();
+
 	response.status(204).end();
 })
 
