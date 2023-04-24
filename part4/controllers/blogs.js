b443e4b487c153e6884804e92ea46e1d34a4a971
Pavel Potemkin <specialforbtsu@gmail.com>
2023-04-24 09:56:04 +0700

    4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -4,14 +4,6 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization');
-	if (authorization && authorization.startsWith('Bearer ')) {
-		return authorization.replace('Bearer ', '');
-	}
-	return null;
-}
-
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
 		.populate('user', { username: 1, name: 1, id: 1 })
@@ -21,7 +13,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body;
 
-	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
 	if (!decodedToken) {
 		return response.status(401).json({ error: 'token invalid' });
 	}
