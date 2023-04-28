10dbd321bd6fabebbaf3635d2621cefb1359c968
Pavel Potemkin <specialforbtsu@gmail.com>
2023-04-28 15:11:18 +0700

    part5.8

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,9 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 
-const Blog = ({blog}) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>  
-)
+const Blog = ({blog}) => {
+	const [visible, setVisible] = useState(false)
+
+	const showWhenVisible = { display: visible ? '' : 'none' }
+	const hideWhenVisible = { display: visible ? 'none' : '' }
+
+
+	const toggleVisibility = () => {
+		setVisible(!visible)
+	}
+
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5
+	}
+
+	return (
+		<div style={blogStyle}>
+			{blog.title}
+			<button style={hideWhenVisible} onClick={toggleVisibility}>show</button>
+			<div style={showWhenVisible}>
+				{blog.author}<br></br>
+				{blog.url}<br></br>
+				{blog.likes}<br></br>
+				<button onClick={toggleVisibility}>hide</button>
+			</div>
+		</div>  
+	)
+}
 
 export default Blog
