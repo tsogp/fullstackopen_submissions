cc1086357c311225000258028771313011ae6a3b
Pavel Potemkin <specialforbtsu@gmail.com>
2023-08-01 23:18:07 +0700

    exercises 6.14-15

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,7 +3,20 @@ import Anecdotes from './components/Anecdotes'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { useEffect } from 'react'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log("her")
+    anecdoteService
+      .getAll().then(notes => dispatch(setAnecdotes(notes)))
+  }, [])
+
   return (
     <>
       <Filter />
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,49 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      console.log(JSON.parse(JSON.stringify(state)))
-      return [...state, {
-          content: action.payload,
-          id: getId(),
-          votes: 0
-      }]
+      state.push(action.payload)
     },
     upVoteAnecdoteById(state, action) {
       const toUpVote = state.find(a => a.id === action.payload)
       const upVoted = {...toUpVote, votes: toUpVote.votes + 1}
-      console.log(JSON.parse(JSON.stringify(state)))
       return state.map(a => 
         a.id !== action.payload ? a : upVoted
       )
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
 
-export const { createAnecdote, upVoteAnecdoteById } = anecdoteSlice.actions
+export const { createAnecdote, upVoteAnecdoteById, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
